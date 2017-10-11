d7e290cc70f70f2a6dbc62881aec1e9e8505628b
chris062689 <chris062689@gmail.com>
2017-10-10 22:23:39 -0400

    Removed the installer folder from the qt-installer repo.

--- installer/build.bat
+++
@@ -1,3 +0,0 @@
-rem Placeholder!
-rem You are going to want to adapt this
-T:\Applications\Qt\Tools\QtInstallerFramework\2.0\bin\binarycreator.exe -c config\config.xml -p empty -n CitraInstaller.exe -v -v -v
